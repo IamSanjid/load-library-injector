400ba31ae77e981956cf062acc2ea1dac483605c
SoraTenshi <dream@neoncity.dev>
2024-01-09 00:02:30 +0100

    Constness

--- src/process.zig
+++ src/process.zig
@@ -4,7 +4,7 @@ const win = std.os.windows;
 const zwin = @import("zigwin32").everything;
 
 pub fn getProcessIdByName(proc_name: [:0]const u16) anyerror!win.DWORD {
-    var handle = zwin.CreateToolhelp32Snapshot(zwin.TH32CS_SNAPPROCESS, 0) orelse return error.InvalidSnapshotHandle;
+    const handle = zwin.CreateToolhelp32Snapshot(zwin.TH32CS_SNAPPROCESS, 0) orelse return error.InvalidSnapshotHandle;
     defer _ = zwin.CloseHandle(handle);
 
     var entry: zwin.PROCESSENTRY32W = undefined;
